b4f293b05e74ce34f0cff80e3760649131c3631c
Dismefront <romaykin.erik@gmail.com>
2023-02-23 20:32:39 +0300

    added jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,17 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('test 1', () => {
+        expect(classNames('a', {}, [])).toBe('a');
+    });
+
+    test('test 2', () => {
+        expect(classNames('a', { throwable: true }, ['fucked', 'bitched']))
+            .toBe('a fucked bitched throwable');
+    });
+
+    test('test 3', () => {
+        expect(classNames('a', {}, ['fucked', 'bitched']))
+            .toBe('a fucked bitched');
+    });
+});
